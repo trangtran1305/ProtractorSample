a2a0fbe7a959f2fe3ae2eb9ba06ea0a56e022d10
trangtran1305 <changtran1305@gmail.com>
2022-09-23 13:39:37 +0700

    Add project files.

---
+++ Demo/conf/conf.js
@@ -0,0 +1,22 @@
+// An example configuration file.
+exports.config = {
+  directConnect: true,
+
+  // Capabilities to be passed to the webdriver instance.
+  capabilities: {
+    'browserName': 'chrome'
+  },
+
+  // Framework to use. Jasmine is recommended.
+  framework: 'jasmine',
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // Spec patterns are relative to the current working directory when
+  // protractor is called.
+  specs: ['../tests/calculator.js'],
+
+  // Options to be passed to Jasmine.
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 30000
+  }
+};
